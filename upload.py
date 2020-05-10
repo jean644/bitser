6bf63c859ba7aeae44e0cadb2bea6a858db7f495
jean644 <64736730+jean644@users.noreply.github.com>
2020-05-10 14:35:36 +0100

    Add files via upload
    Single config

--- upload.py
+++ upload.py
@@ -1,8 +1,21 @@
 import logging
 import boto3
+import os
 from botocore.exceptions import ClientError
+from configparser import ConfigParser
 
 def upload(fileName):
+    #Read config.ini file
+    config_object = ConfigParser()
+    config_object.read("config.ini")
+    #Get the key and secret
+    awsDetails = config_object["aws"]
+    os.environ["AWS_ACCESS_KEY_ID"] = awsDetails["key"]
+    os.environ["AWS_SECRET_ACCESS_KEY"] = awsDetails["secret"]
+    
+    
+    
+    
     # Let's use Amazon S3
     s3 = boto3.resource('s3')
 
@@ -11,3 +24,5 @@ def upload(fileName):
     data = open(fileName, 'rb')
     s3.Bucket('mmvm-1').put_object(Key=fileName, Body=data)
     print ('Uploaded')
+
+#upload ('output.jpeg')
